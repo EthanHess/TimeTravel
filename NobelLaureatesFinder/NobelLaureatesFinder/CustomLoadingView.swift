ddaa8db13b1acd5a8b8177e7173a9c634934c3ff
ech1988@gmail.com <ech1988@gmail.com>
2020-05-25 19:07:38 -0700

    Custom loading view updates

--- NobelLaureatesFinder/NobelLaureatesFinder/CustomLoadingView.swift
+++ NobelLaureatesFinder/NobelLaureatesFinder/CustomLoadingView.swift
@@ -39,6 +39,11 @@ class CustomLoadingView: UIView {
         return timer
     }()
     
+    var borderTimer : Timer = {
+        let bt = Timer()
+        return bt
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -47,7 +52,7 @@ class CustomLoadingView: UIView {
     //Public
     func visibilityHandler(show: Bool) {
         DispatchQueue.main.async {
-            self.isHidden = show
+            self.isHidden = !show
         }
     }
     
@@ -55,9 +60,37 @@ class CustomLoadingView: UIView {
         addSubview(loadingLabel)
         loadingLabel.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 5, paddingBottom: 5, paddingRight: 5, width: 0, height: 30)
         addSubview(pieChart)
-        pieChart.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 5, paddingLeft: 5, paddingBottom: 0, paddingRight: 5, width: 0, height: 0)
-        
+        pieChart.anchor(top: topAnchor, left: leftAnchor, bottom: loadingLabel.topAnchor, right: rightAnchor, paddingTop: 7.5, paddingLeft: 5, paddingBottom: 7.5, paddingRight: 5, width: 90, height: 90)
+        pieChart.layer.borderColor = colorsForBorder()[0].cgColor
+        pieChart.layer.borderWidth = 1
+
         stylizeSelf()
+        borderColorTimerConfiguration()
+    }
+    
+    fileprivate func borderColorTimerConfiguration() {
+        borderTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(borderForPieChart), userInfo: nil, repeats: true)
+    }
+    
+    //Will animate/change color
+    @objc fileprivate func borderForPieChart() {
+        let x = Int(arc4random() % 8);
+        let borderColor = colorsForBorder()[x]
+        pieChart.layer.borderColor = borderColor.cgColor
+        pieChart.layer.borderWidth = 1
+        //Use for fill colors as well?
+    }
+    
+    fileprivate func colorsForBorder() -> [UIColor] {
+        let colorOne = UIColor.fromRGB(red: 124, green: 247, blue: 252)
+        let colorTwo = UIColor.fromRGB(red: 124, green: 252, blue: 230)
+        let colorThree = UIColor.fromRGB(red: 124, green: 199, blue: 252)
+        let colorFour = UIColor.fromRGB(red: 13, green: 236, blue: 194)
+        let colorFive = UIColor.fromRGB(red: 13, green: 126, blue: 236)
+        let colorSix = UIColor.fromRGB(red: 177, green: 175, blue: 248)
+        let colorSeven = UIColor.fromRGB(red: 227, green: 175, blue: 248)
+        let colorEight = UIColor.fromRGB(red: 246, green: 101, blue: 159)
+        return [colorOne, colorTwo, colorThree, colorFour, colorFive, colorSix, colorSeven, colorEight]
     }
     
     fileprivate func stylizeSelf() {
@@ -79,6 +112,11 @@ class CustomLoadingView: UIView {
     
     @objc fileprivate func animateMiniChart() {
         //TODO imp.
+    }
+    
+    //Destroy
+    deinit {
+        borderTimer.invalidate()
     }
     
     required init?(coder: NSCoder) {
